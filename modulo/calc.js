e2f32e1f8847f05c3bbf1ef9d81d1fadb30f376d
Sarah Cristina Ribeiro <100208442+projetoSarahRibeiro@users.noreply.github.com>
2023-03-14 11:09:56 -0300

    Add files via upload

---
+++ modulo/calc.js
@@ -0,0 +1,10 @@
+const somaF = require('./soma')
+const divF = require('./div')
+const multF = require ('./mult')
+const subF = require ('./sub')
+
+console.log("A soma é: " + somaF(3,3))
+console.log("A div é: " + divF(3,3))
+console.log("A multiplicação é: " + divF(3,3))
+console.log("A subtração é: " + subF(3,3))
+
